64bb01dd1d6f10b983bb66d29ab2af1712b78c97
Rui Wang <harry.r.wang@gmail.com>
2014-11-10 14:26:06 +0000

    added assembly configuration and ConfigurationPrinter to print out hadoop configurations

---
+++ src/main/java/org/harrywang/hadoop/util/ConfigurationPrinter.java
@@ -0,0 +1,41 @@
+package org.harrywang.hadoop.util;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.util.Map;
+
+/**
+ * Print out all the configuration for the local hadoop installation
+ *
+ * This class is the original code from "Hadoop: The Definitive Guide"
+ *
+ * @author Rui Wang
+ * @version $Id$
+ */
+public class ConfigurationPrinter extends Configured implements Tool {
+
+    static {
+        Configuration.addDefaultResource("hdfs-default.xml");
+        Configuration.addDefaultResource("hdfs-site.xml");
+        Configuration.addDefaultResource("mapred-default.xml");
+        Configuration.addDefaultResource("mapred-site.xml");
+    }
+
+    @Override
+    public int run(String[] strings) throws Exception {
+        Configuration conf = getConf();
+        for (Map.Entry<String, String> entry : conf) {
+            System.out.printf("%s=%s\n", entry.getKey(), entry.getValue());
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new ConfigurationPrinter(), args);
+        System.exit(exitCode);
+    }
+}
